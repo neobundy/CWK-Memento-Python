35a3da8b9db7f3babf4a62f5591eb77eea3dd9f5
Wankyu Choi <wankyuchoi@gmail.com>
2019-03-22 06:07:42 +0900

    Blocks and Scopes

--- zen_of_python.py
+++ zen_of_python.py
@@ -1,1 +1,26 @@
-import this
+#!/usr/bin/env python3
+# Wankyu Choi - Creative Works of Knowledge 2019
+# https://www.youtube.com/wankyuchoi
+
+import platform  # importing platform package
+
+
+def zen_of_python():
+    """Print the Zen of Python"""
+
+    import this
+
+
+def main():
+    """Entry Point"""
+
+    if True:
+        print("Hello there!")
+        print("Good to see ya!")
+
+    print("You're on {}", platform.system())
+    print("You have Python Version {}.".format(platform.python_version()))
+
+
+if __name__ == '__main__':
+    main()
