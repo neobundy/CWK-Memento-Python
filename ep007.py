44467fa37d79a7c9511b862aec403a767608c5a2
Wankyu Choi <wankyuchoi@gmail.com>
2019-03-28 10:57:13 +0900

    EP007 - Version Control and Git

---
+++ ep007.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Wankyu Choi - Creative Works of Knowledge 2019
+# https://www.youtube.com/wankyuchoi
+#
+# Episode 007 - Version Control and Git
+#
+
+
+#
+#   No coding necessary for this episode.
+#
+
+def main():
+    """Entry Point"""
+
+    print("Just sit back and enjoy. No coding necessary for this episode.")
+    print("This file just sits idle for the sake of completeness.")
+
+
+if __name__ == '__main__':
+    main()
+
+
